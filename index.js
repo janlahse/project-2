e0c8ff13b40d8d07486587022f601a082e6d29f5
Jan <jan.lahse@gmx.de>
2025-04-09 11:33:50 +0200

    added eventlistener to show answer button to toggle answer visibility and button label

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("bookmark--active");
 });
 
-//card__answer--active
+//toggle answer visibility & button caption on click
+const answerButton = document.querySelector("[data-js=answer-button]");
+const answer = document.querySelector("[data-js=answer]");
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+  if (answer.classList.contains("card__answer--active")) {
+    answerButton.innerHTML = "Hide answer";
+  } else {
+    answerButton.innerHTML = "Show answer";
+  }
+});
